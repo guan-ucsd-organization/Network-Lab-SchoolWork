3de49fcd359767da3e73419cf4157d3e0d64d3d3
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2020-11-30 00:01:41 -0800

    Serialized directly from file, couple bugs....

--- src/DeserializeStudent.java
+++ src/DeserializeStudent.java
@@ -9,44 +9,72 @@ public class DeserializeStudent {
 
 
         Student student = null;
+        byte[] objByte = null;
 
+        ServerSocket serverSocket = new ServerSocket(5000); //Create a server socket
+
+        Socket socket = serverSocket.accept(); //Listen for connection request;
+
+        //create data input and output streams;
+        DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+
+        while(true){
+            objByte = inputFromClient.readAllBytes();
+            if (objByte != null){
+                break;
+            }
+        }
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(objByte);
+        ObjectInput in = null;
         try {
-            FileInputStream fileInputStream = new FileInputStream("student.ser");
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            student = (Student) objectInputStream.readObject();
-            objectInputStream.close();
-            fileInputStream.close();
+            in = new ObjectInputStream(bis);
+            Student o = (Student) in.readObject();
+
+            System.out.println("Deserialized Student...");
+            System.out.println("Name: " + o.getName());
+            System.out.println("Age: " + o.getAge());
+            System.out.println("Grades: " + o.getGrades());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                // ignore close exception
+            }
         }
-        catch (IOException i) {
-            i.printStackTrace();
-            return;
-        }
-        catch (ClassNotFoundException c) {
-            System.out.println("Student class not found");
-            c.printStackTrace();
-            return;
-        }
-
-        System.out.println("Deserialized Student...");
-        System.out.println("Name: " + student.getName());
-        System.out.println("Age: " + student.getAge());
-        System.out.println("Grades: " + student.getGrades());
 
 
 
-//        ServerSocket serverSocket = new ServerSocket(5000); //Create a server socket
-//
-//        Socket socket = serverSocket.accept(); //Listen for connection request;
-//
-//        //create data input and output streams;
-//        DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-//        DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-//
-//        while(true){
-//            int number = inputFromClient.readInt();
-//            int returnNumber = number * 2;
-//            outputToClient.writeInt(returnNumber);
+
+
+
+//        try {
+//            FileInputStream fileInputStream = new FileInputStream("student.ser");
+//            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+//            student = (Student) objectInputStream.readObject();
+//            objectInputStream.close();
+//            fileInputStream.close();
 //        }
+//        catch (IOException i) {
+//            i.printStackTrace();
+//            return;
+//        }
+//        catch (ClassNotFoundException c) {
+//            System.out.println("Student class not found");
+//            c.printStackTrace();
+//            return;
+//        }
+
+
+
+
+
+
+
 
     }
 }
--- src/SerializeStudent.java
+++ src/SerializeStudent.java
@@ -3,6 +3,8 @@ import java.net.*;
 import java.io.*;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 
@@ -11,38 +13,58 @@ public class SerializeStudent {
     public static void main(String[] args) throws IOException {
 
         Student S = new Student("Jerry", 10 , 8);
-        try{
-            FileOutputStream fileOutputStream = new FileOutputStream("student.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOutputStream);
+
+        //This is the part for Serializing the Student object in the November22Lab directories.
+//        try{
+//            FileOutputStream fileOutputStream = new FileOutputStream("student.ser");
+//            ObjectOutputStream out = new ObjectOutputStream(fileOutputStream);
+//            out.writeObject(S);
+//            out.close();
+//            fileOutputStream.close();
+//            System.out.printf("Serialized data is saved in src");
+//        }
+//        catch(IOException i){
+//            i.printStackTrace();
+//        }
+
+
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(bos);
             out.writeObject(S);
-            out.close();
-            fileOutputStream.close();
-            System.out.printf("Serialized data is saved in src");
+            out.flush();
+            byte[] yourBytes = bos.toByteArray();
+            //convert Btyes to string
+            //String s = new String(yourBytes , StandardCharsets.UTF_8);
+
+
+
+
+            DataOutputStream toServer = null;
+            DataInputStream fromServer = null;
+
+            Socket socket = new Socket("localhost", 5000);
+
+            fromServer = new DataInputStream(socket.getInputStream());
+            toServer = new DataOutputStream(socket.getOutputStream());
+
+            toServer.write(yourBytes); //send s in as bytes
+            toServer.flush();
+
+//            int answer = fromServer.readInt();
+//            System.out.println("The answer is " + answer);
+
+
+
+
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException ex) {
+                // ignore close exception
+            }
         }
-        catch(IOException i){
-            i.printStackTrace();
-        }
-
-//
-//        DataOutputStream toServer = null;
-//        DataInputStream fromServer = null;
-//
-//        int number = 5;
-//
-//        Socket socket = new Socket("localhost", 5000);
-//
-//        fromServer = new DataInputStream(socket.getInputStream());
-//        toServer = new DataOutputStream(socket.getOutputStream());
-//
-//        //send the number to the server
-//        toServer.writeInt(number);
-//        toServer.flush();
-//
-//        //get the double value from the server.
-//        int answer = fromServer.readInt();
-//
-//        System.out.println("The answer is " + answer);
-
-
     }
 }
